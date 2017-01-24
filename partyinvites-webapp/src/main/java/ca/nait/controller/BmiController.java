868901e960b266dbb5dacd021eb1795ae5351d58
renzelle frank rodrigueza <renzelle_rodrigueza@yahoo.ca>
2017-01-23 19:52:20 -0800

    Creating CSS Style and using 3rd party helpers

--- partyinvites-webapp/src/main/java/ca/nait/controller/BmiController.java
+++ partyinvites-webapp/src/main/java/ca/nait/controller/BmiController.java
@@ -4,31 +4,39 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+
+import org.omnifaces.util.Messages;
 
 @Named // allows you to access an instance of this class using JSF-EL (Expression Language)
 @RequestScoped // create an instance of this class for one HTTP request
 public class BmiController {
 	
-	private double weight; //getter and setter
-	private double height; // getter and setter
+	@NotNull(message="Please enter a weight.")
+	private Double weight; //getter and setter
 	
-	public double getWeight() {
+	@NotNull(message="Please enter a height.")
+	private Double height; // getter and setter
+	
+	
+
+	
+	public Double getWeight() {
 		return weight;
 	}
-	
-	public void setWeight(double weight) {
+
+	public void setWeight(Double weight) {
 		this.weight = weight;
 	}
-	
-	public double getHeight() {
+
+	public Double getHeight() {
 		return height;
 	}
-	
-	public void setHeight(double height) {
+
+	public void setHeight(Double height) {
 		this.height = height;
 	}
 
-	
 	private double calculateBMI(double weight, double height)
 	{
 		return 703.0 * weight / Math.pow(height, 2);
@@ -38,14 +46,35 @@ public class BmiController {
 	{
 		double bmiValue = this.calculateBMI(this.weight, this.height);
 		// Send info message to FacesContext
-		String message = String.format("Your BMI is %.1f", bmiValue);
-		FacesContext facesContext = FacesContext.getCurrentInstance();
+		
+		//FacesContext facesContext = FacesContext.getCurrentInstance();
 		// Send message to FaceMessage
-		FacesMessage facesMessage = new FacesMessage(message);
+		//FacesMessage facesMessage = new FacesMessage(message);
 		// Set severity
-		facesMessage.setSeverity(FacesMessage.SEVERITY_INFO);
+		//facesMessage.setSeverity(FacesMessage.SEVERITY_INFO);
 		// set FaceContext with the created faceMessage
-		facesContext.addMessage(null, facesMessage);
+		//facesContext.addMessage(null, facesMessage);
+		// Using omnifaces
+		if( bmiValue < 18.5)
+		{
+			String message = String.format("Your BMI is %.1f. You are underweight", bmiValue);
+			Messages.addFlashGlobalWarn(message);
+		}
+		else if (bmiValue >= 18.5 && bmiValue <= 24.99)
+		{
+			String message = String.format("Your BMI is %.1f. You are at optimal weight", bmiValue);
+			Messages.addFlashGlobalInfo(message);
+		}
+		else if (bmiValue >= 25 && bmiValue < 30)
+		{
+			String message = String.format("Your BMI is %.1f. You are overweight", bmiValue);
+			Messages.addFlashGlobalError(message);
+		}
+		else
+		{
+			String message = String.format("Your BMI is %.1f. You are on health trouble", bmiValue);
+			Messages.addFlashGlobalFatal(message);
+		}
 	}
 	
 	@Override
